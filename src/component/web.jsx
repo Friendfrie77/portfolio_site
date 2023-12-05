934ac395816296c162f2c777a27e64f906e52a31
Friendfrie77 <awfriend77@gmail.com>
2023-12-05 15:28:25 -0500

    updating to switch computers

--- src/component/web.jsx
+++ src/component/web.jsx
@@ -191,6 +191,29 @@ function Web(){
                     </div>
                     <hr className='content-hr hr-dark'></hr>
                 </li>
+                <li className="panel project-three web-four">
+                    <div className='panel-description'>
+                        <div>
+                            <h3 className='project-title'>JobsJustForYou WordPress XML Parser Plugin</h3>
+                            <img className='panel-description-img' src={OOP} height="100%" width='700' alt="Sql Final"/>
+                            <div className='project-description'>
+                                <p>For this project, I was tasked with making a full application using OOP to make a bike rental app. While the base classes were given, I heavily modified and added to the classes for them to fit the specifications given. I coded and debugged the program over the course of a week. You can find the code in the GitHub link below.</p>
+                            </div>
+                            <ul className='tech-list'>
+                                <li>PHP</li>
+                                <li>WordPress</li>
+                                <li>SQL</li>
+                            </ul>
+                            <div className='project-links'>
+                                <a href='https://github.com/XML-JJFY/xml-plugin/tree/main' title='GitHub link' target='_blank' rel="noreferrer"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill='white' viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='panel-picture'>
+                        <img src={OOP} height="100%" width='700' alt='OOP final'/>
+                    </div>
+                    <hr className='content-hr hr-light'></hr>
+                </li>
                 <li className="panel project-two web-two">
                     <div className='panel-description'>
                         <div>
